a125f1085575f314d592bd9a7a0bf1642ebcd88a
jaeyo03 <toyazx@naver.com>
2024-12-20 01:46:08 +0900

    fix : 최신 문서가 없을때 기본 UI 설정

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -53,24 +53,33 @@ async function render(path, query){
       }
     })
   )
-  
+
   return `
     <div id="welcomeContents" class="my-5 mx-auto text-left col-lg-10">
         <h2 class="fw-semibold text-center mb-4">${userName}님, 안녕하세요.</h2>
         <div class="my-4">
           <p class="fs-6"><i class="bi bi-clock-history fs-6"></i> <span class="ms-1">최근 파일</span></p>
-          <div class="d-grid gap-2">
-          ${
+          ${latestDocs.length > 0 ?
             latestDocs.map((doc, idx)=>{
               return `
-              <div class="list border rounded-2 p-3 col" data-id="${doc.id}">
-                  <h6 class="text-truncate">${doc.title}</h6>
-                  <blockquote class="mb-1 fs-6 fw-light lh-sm">${doc.content.replace(/<[^>]+>/g,"").slice(0,500)}</blockquote>
-                  <span class="updated">${updateTimeCalc(doc.updatedAt)}</span>
-              </div>`
-            }).join("")
+                <div class="d-grid gap-2">
+                  <div class="list border rounded-2 p-3 col" data-id="${doc.id}">
+                    <h6 class="text-truncate">${doc.title}</h6>
+                    <blockquote class="mb-1 fs-6 fw-light lh-sm">${doc.content?.replace(/<[^>]+>/g,"").slice(0,500)}</blockquote>
+                    <span class="updated">${updateTimeCalc(doc.updatedAt)}</span>
+                  </div>
+                </div>
+              `
+            }).join("") : 
+            `
+            <div>
+              <div class="list border rounded-2 p-3" style="pointer-events: none; align-content: center; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+                <h6 class="text-truncate">새로운 문서를 작성해보세요!</h6>
+                <blockquote class="mb-1 fs-6 fw-light lh-sm" style="text-align: center; margin: 0;">아래 글쓰기 버튼을 클릭해보세요!</blockquote>
+              </div>
+            </div>
+            `
           }
-          </div>
         </div>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <button id="write1" type="button" class="btn btn-lg px-4 gap-3 fs-6">글쓰기</button>
